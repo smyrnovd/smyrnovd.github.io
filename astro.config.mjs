9be574dd64eae3b7423b363a370c5eb0a9652bad
smyrnovd <smyrnovd@gmail.com>
2024-02-20 01:08:34 +0200

    GA added and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), preact()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: "https://smyrnov.eu",
 });
