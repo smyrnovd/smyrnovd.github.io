bebf64e7dbb10e988c87385e8b48efd29b47e74d
smyrnovd <smyrnovd@gmail.com>
2024-04-11 17:46:02 +0300

    added daisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://smyrnov.eu",
-  integrations: [
-    mdx(),
-    sitemap(),
-    preact(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
-  site: "https://smyrnov.eu",
+  integrations: [mdx(), sitemap(), preact(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind()],
+  site: "https://smyrnov.eu"
 });
