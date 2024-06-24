93ef0752016c039c4e947afc0e8430d01dfca4c5
Dmitry Smyrnov <smyrnovd@gmail.com>
2024-06-24 21:53:47 +0300

    new view

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Software Develop | Dmitry Smyrnov';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Smyrnov.eu';
+export const SITE_DESCRIPTION = 'Software Develop | Dmitry Smyrnov';
