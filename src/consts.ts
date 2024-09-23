d207d04cbc7ab76cf16a432968abc304d4658496
Dmitry Smyrnov <smyrnovd@gmail.com>
2024-09-23 13:10:21 +0300

    update  name and libraries

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Smyrnov.eu';
-export const SITE_DESCRIPTION = 'Software Develop | Dmitry Smyrnov';
+export const SITE_TITLE = "smyrnov.eu";
+export const SITE_DESCRIPTION = "Software Develop | Dmytro Smyrnov";
